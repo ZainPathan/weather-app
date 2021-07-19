83977ee71d6055975c8608baa1a0874005299f51
ZainPathan <ZPA4573>
2021-07-19 07:11:00 +0530

    Updated for deployment to GitHub pages and fetch data

--- src/redux/modules/weather/actions.ts
+++ src/redux/modules/weather/actions.ts
@@ -11,7 +11,7 @@ import {
     convertTemperatureToFahrenheit
 } from '../../../core/utils/TemperatureConversions';
 
-const GET_WEATHER_DATA_URL: string = '/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40';
+const GET_WEATHER_DATA_URL: string = 'https://thingproxy.freeboard.io/fetch/http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40';
 
 export const getWeatherDataRequestStart = () => ({
     type: GET_WEATHER_DATA_REQUEST_START
